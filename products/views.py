8634eed725f2da24220d7b44f0c884b09e6e5de9
Dan Boots <thestereojackers@gmail.com>
2021-01-26 15:46:16 +0000

    going to overright

--- products/views.py
+++ products/views.py
@@ -3,43 +3,16 @@ from .models import Product
 
 # Create your views here.
 
+
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
 
     context = {
-        'products': products,
+        "products": products,
     }
 
-    return render(request, 'products/products.html', context)
-
-
-def product_detail(request, product_id):
-    """ A view to show individual product details """
-
-    product = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        'product': product,
-    }
-
-<<<<<<< HEAD
-    return render(request, 'products/product_detail.html', context)
-=======
-    return render(request, "products/products.html", context)
-
-
-def product_detail(request, product_id):
-    """ A view to show individual product details """
-
-    product = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        "product": product,
-    }
-
-<<<<<<< HEAD
     return render(request, "products/products.html", context)
 
 
@@ -53,7 +26,3 @@ def product_detail(request, product_id):
     }
 
     return render(request, "products/product_detail.html", context)
-=======
-    return render(request, "products/product_detail.html", context)
->>>>>>> 44ff4fb7513d611ee2e83802ff77d9503708f35c
->>>>>>> eb4ac464ba5909b61a61f9d8d0e63e256febebe4
