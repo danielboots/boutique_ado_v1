3b8e260c0c8ecfcd7359874b9bdfc2086532ee92
Dan Boots <thestereojackers@gmail.com>
2021-01-26 15:56:16 +0000

    brand new

--- boutique_ado_v1/urls.py
+++ boutique_ado_v1/urls.py
@@ -1,7 +1,7 @@
-"""boutique_ado_v1 URL Configuration
+"""boutique_ado URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,8 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.urls")),
-    path("", include("home.urls")),
-    path("products/", include("products.urls")),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
